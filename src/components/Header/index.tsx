4006794a1c66386434a9fb61cb8904b92fe06388
Paulo Henrique <phvcandido@gmail.com>
2021-05-16 10:41:09 -0300

    Ajustando ambiente para desenvolvimento
    Adicionando router, estilos, header e footer

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './style.module.scss';
+function Header() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Produtos/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './style.module.scss';
+function Produtos() {
+  return (
+    <div>
+      <h1>Produtos</h1>
+    </div>
+  )
+}
+
+export default Produtos;
