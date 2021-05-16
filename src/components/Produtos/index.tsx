4d50976b2ca544a70c2571d3e914cce8a5cd64dc
Paulo Henrique <phvcandido@gmail.com>
2021-05-16 11:20:36 -0300

    Ajustando estrutura de Produtos

--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Head from '../Head';
 import style from './style.module.scss';
+
+interface Produto {
+  id: string;
+  nome: string;
+  fotos: [{ src: string; titulo: string; }];
+}
 function Produtos() {
+  const [produtos, setProdutos] = React.useState([]);
+  React.useEffect(() => {
+
+    try {
+      fetch('https://ranekapi.origamid.dev/json/api/produto').then((r) => r.json()).then((json) => setProdutos(json));
+      fetch('https://ranekapi.origamid.dev/json/api/produto').then((r) => r.json()).then((json) => setProdutos(json));
+    } catch (err) {
+      console.log(err);
+    }
+
+  }, []);
+
+
+  if (produtos === []) return null;
+
   return (
-    <div>
+    <section className={style.produtos + 'animeLeft'}>
       <Head title="Produtos" description="Página de contato" />
-      <h1>Produtos</h1>
-    </div>
+      {produtos.map((p: Produto) => (
+        <Link to={`produto/${p.id}`} key={p.id}>
+          <img src={p.fotos[0].src} alt={p.fotos[0].titulo} />
+          <h1>{p.nome}</h1>
+        </Link>
+      ))}
+    </section>
   )
 }
 
